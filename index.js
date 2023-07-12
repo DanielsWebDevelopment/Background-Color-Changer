6b4ad6ee0b1b6c5c9e5ff0e26352049ed47889cf
Daniel <daniel23504209@gmail.com>
2023-07-12 13:10:03 -0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function btn(z) {
-    var a = getComputedStyle(z);
+function btn(z){
+    var a = getComputedStyle(z)
     var b = a.backgroundColor;
     document.getElementsByTagName("body")[0].style.backgroundColor = b;
 }
